a8c7118dbaf7af3c5926c1d7cf3c83cd0dfe89f4
dxxwls <eunjin0468@naver.com>
2025-07-17 11:27:14 +0900

    Initial commit

---
+++ kiosk/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean flag = true;
+        while (flag){
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료           | 종료");
+            int choiceMenu = scanner.nextInt();
+
+            switch (choiceMenu) {
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    flag = false;
+                    break;
+                case 1:
+                    System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                    break;
+                case 2:
+                    System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                    break;
+                case 3:
+                    System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                    break;
+                case 4:
+                    System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                    break;
+                default:
+                    System.out.println("잘못된 값이 입력되었습니다.");
+            }
+        }
+        scanner.close();
+    }
+}
